92063771e162c1a40c8182341dd87de8e463cc5d
prometeh <prometeh.zanjir@gmail.com>
2022-06-13 12:21:17 +0200

    lab-3: added the useFetch custom hook

---
+++ lab_3/solutions/parcel/src/scripts/hooks/useFetch.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(undefined);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(undefined);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not get the data from the database!");
+        }
+
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+        setError(undefined);
+      })
+      .catch((e) => {
+        setLoading(false);
+        setError(e.message);
+        console.log(e.message);
+      });
+  }, [url]);
+
+  return [data, loading, error];
+};
+
+export default useFetch;
